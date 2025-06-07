bee210237ab3958da8e0df1eee42a33793e196c7
Taufiq Septryana <taufiqseptry17@gmail.com>
2025-06-07 13:30:28 +0700

    refactor(db): set open conn, conn pool

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 )
 
 require (
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/sys v0.33.0 // indirect
--- internal/db/db.go
+++ internal/db/db.go
@@ -2,9 +2,27 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
 func New(host, port, customer, pass, name string) (*sql.DB, error) {
-	// Connect to the database here
-	return &sql.DB{}, nil
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, customer, pass, name)
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(50)
+	db.SetMaxIdleConns(25)
+	db.SetConnMaxIdleTime(time.Minute * 5)
+	db.SetConnMaxLifetime(time.Hour)
+
+	return db, nil
 }
