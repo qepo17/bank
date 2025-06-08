06be2392843f157326c15bb35f05c4503362e82b
Taufiq Septryana <taufiqseptry17@gmail.com>
2025-06-08 13:11:25 +0700

    chore: remove uneeded files

--- entity/customer.go
+++
@@ -1,7 +0,0 @@
-package entity
-
-type Customer struct {
-	Model
-	Email    string
-	Password string
-}
--- http/handler/customer/auth.go
+++
@@ -1,9 +0,0 @@
-package customer
-
-import "net/http"
-
-func (h *Handler) Login() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Login Customer"))
-	}
-}
